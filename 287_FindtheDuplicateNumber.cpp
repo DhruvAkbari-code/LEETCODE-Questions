3f2b7cb8709ec10b8bf1d9abc903e6538b2f4dc5
Dhruv <ad.dhruvakbari@gmail.com>
2026-03-27 15:06:10 +0530

    Leetcode Question 2965,287.

---
+++ 287_FindtheDuplicateNumber.cpp
@@ -0,0 +1,51 @@
+// TODO: Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
+// TODO: There is only one repeated number in nums, return this repeated number.
+// TODO: You must solve the problem without modifying the array nums and using only constant extra space.
+
+//* Example 1:
+// Input: nums = [1,3,4,2,2]
+// Output: 2
+
+//* Example 2:
+// Input: nums = [3,1,3,4,2]
+// Output: 3
+
+//* Example 3:
+// Input: nums = [3,3,3,3,3]
+// Output: 3
+
+//? Constraints:
+// 1 <= n <= 105
+// nums.length == n + 1
+// 1 <= nums[i] <= n
+// All the integers in nums appear only once except for precisely one integer which appears two or more times.
+
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+using namespace std;
+int main()
+{
+    vector<int> nums = {1, 3, 4, 2, 2};
+    // unordered_set<int> s;
+    // for(int i = 0; i < nums.size(); i++){
+    //     if(s.find(nums[i]) != s.end()){
+    //         cout<< nums[i];
+    //     }
+    //     s.insert(nums[i]);
+    // }
+    // return 0;
+
+    int n = nums.size();
+    vector<int> freq(n, 0);
+
+    for (int i = 0; i < n; i++)
+    {
+        freq[nums[i]]++;
+        if (freq[nums[i]] == 2)
+        {
+            cout << nums[i];
+        }
+    }
+    return 0;
+}
