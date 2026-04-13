a9a06200dfb72986a619bef2b15f66b1b890615a
Dhruv <ad.dhruvakbari@gmail.com>
2026-04-13 12:57:46 +0530

    Leetcode Question 1337, 2529, 349.

--- 349_IntersectionofTwoArrays.cpp
+++ 349_IntersectionofTwoArrays.cpp
@@ -1,39 +1,42 @@
+// TODO: Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must be unique and you may return the result in any order.
 
-// Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must be unique and you may return the result in any order.
-
-// Example 1:
-
+// *Example 1:
 // Input: nums1 = [1,2,2,1], nums2 = [2,2]
 // Output: [2]
-// Example 2:
 
+// *Example 2:
 // Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
 // Output: [9,4]
 // Explanation: [4,9] is also accepted.
-// ! NOT DONE YET
+
+// ?Constraints:
+// 1 <= nums1.length, nums2.length <= 1000
+// 0 <= nums1[i], nums2[i] <= 1000
+
 #include <iostream>
 #include <vector>
+#include <unordered_set>
 using namespace std;
 int main()
 {
     vector<int> nums1 = {1, 2, 2, 1};
     vector<int> nums2 = {2, 2};
-    vector<int> values;
+    unordered_set<int> set1(nums1.begin(), nums1.end());
+    unordered_set<int> result;
 
-    for (int i = 0; i < nums1.size(); i++)
+    for (int num : nums2)
     {
-        for (int j = 0; j < nums2.size(); j++)
+        if (set1.count(num))
         {
-            if (nums1[i] == nums2[j])
-            {
-                values.push_back(nums1[i]);
-            }
+            result.insert(num);
         }
     }
 
-    for (int a : values)
+    vector<int> ans = vector<int>(result.begin(), result.end());
+
+    for (int i = 0; i < ans.size(); i++)
     {
-        cout << a << ":" << endl;
+        cout << ans[i];
     }
 
     return 0;
